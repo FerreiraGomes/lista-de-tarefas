4f2bf80f33c0a37b7570ef183d145431fe89edf7
gabriel <gabrielpcnet53@gmail.com>
2023-02-24 22:37:07 -0300

    commit alterado

--- main.js
+++ main.js
@@ -2,15 +2,13 @@ $(document).ready(function() {
 
     $('form').on('submit', function(e){
         e.preventDefault();
-
         const novaTarefa = $("#tarefas").val(); //esse é o valor colocado no input e guardando na vaiavel novaTarefa
         const novoItem = $('<li></li>'); //aqui criamos o elemento e guardamos na variavel novoItem
     
-        $("{novaTarefa}").appendTo(novoItem); //estamos adicionando <li> para o nosso elemento .appendTo(novoItem)
+        $(`<li>${novaTarefa}</li>`)     //Aqui estamos add o elemento novaTarefa ao novoItem 
+        .appendTo(novoItem)
+        .click(() => $(event.target).toggleClass('riscado')); //Aqui está a função que aplica na class riscado para add ou remover o risco 
         $(novoItem).appendTo('ul');
-        $("li").click (function () {
-            $(this).addClass("riscado");
-        });
-        $('#lista-tarefas').val("");
+        $('#tarefas').val("")
     })
 })
